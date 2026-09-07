57b75f1517734d657b1afe0031b37bd1dcfd30ac
Tobias Unruh <tobias.unruh94@gmail.com>
2026-09-07 18:13:15 +0000

    fix: correct URL to version.txt file

--- include/ota_config.h
+++ include/ota_config.h
@@ -2,11 +2,7 @@
 
 struct OtaConfig
 {
-    // static constexpr const char *VERSION_URL = "https://github.com/TobiU94/iot_esp8266_wifi_controlled_fairy_lights/releases/latest/download/version.txt";
-
-    static constexpr const char *VERSION_URL =
-        "https://github.com/TobiU94/iot_esp8266_wifi_controlled_fairy_lights/releases/download/v31/version.txt";
-
+    static constexpr const char *VERSION_URL = "https://github.com/TobiU94/iot_esp8266_wifi_controlled_fairy_lights/releases/latest/download/version.txt";
     static constexpr const char *FIRMWARE_URL = "https://github.com/TobiU94/iot_esp8266_wifi_controlled_fairy_lights/releases/latest/download/firmware.bin";
     static constexpr unsigned long CHECK_INTERVAL_MS = 10000; // 60000;
 };
